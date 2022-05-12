d1df6c4a276fc7f323c2721572392ccaefd11f22
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2022-05-12 21:29:02 +0100

    Fixed autopull by using user to pull

--- _build/autopull.sh
+++ _build/autopull.sh
@@ -8,13 +8,13 @@ cd $(dirname "$0")
 
 echo "$(date --iso-8601=seconds) GH fetch"
 
-su gnoel -c git fetch
+su gnoel -c 'git fetch'
 h1=$(git rev-parse HEAD)
 h2=$(git rev-parse @{u})
 
 if [[ $h1 != $h2 ]]; then
   echo "$(date --iso-8601=seconds) GH merge + TF clear"
-  su gnoel -c git merge && service trafficserver stop && traffic_server -Cclear && service trafficserver start
+  su gnoel -c 'git merge' && service trafficserver stop && traffic_server -Cclear && service trafficserver start
   if [[ $? -eq 0 ]]; then
     echo "done"
   else
