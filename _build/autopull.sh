1391d1042df16ccb1666d769541fd5288eda3372
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2022-05-13 00:52:00 +0100

    Fixed autopull.sh so it works well with git owner & root for TS restart.

--- _build/autopull.sh
+++ _build/autopull.sh
@@ -3,21 +3,31 @@
 # If there is a change, clear traffice server cache.
 # This script is supposed to be executed by a root cron job.
 
+# the user who owns the git repo
+GITUSER="jeff"
+
 # cd where this script is
 cd $(dirname "$0")
 
 echo "$(date --iso-8601=seconds) GH fetch"
 
-su gnoel -c 'git fetch'
-h1=$(git rev-parse HEAD)
-h2=$(git rev-parse @{u})
+su $GITUSER -c 'id && git fetch'
+echo "$?"
+if [[ $? -ne 0 ]]; then
+  echo "failed: git fetch"
+  exit
+fi
 
-if [[ $h1 != $h2 ]]; then
+h1=$(su $GITUSER -c 'git rev-parse HEAD')
+h2=$(su $GITUSER -c 'git rev-parse @{u}')
+
+if [[ $h1 == $h2 ]]; then
   echo "$(date --iso-8601=seconds) GH merge + TF clear"
-  su gnoel -c 'git merge' && service trafficserver stop && traffic_server -Cclear && service trafficserver start
+  su $GITUSER -c 'git merge' && service trafficserver stop && traffic_server -Cclear && service trafficserver start
+  # su $GITUSER -c 'git merge' && service cron stop && service cron start
   if [[ $? -eq 0 ]]; then
     echo "done"
   else
-    echo "failed"
+    echo "failed: git merge OR TS restart"
   fi
 fi
