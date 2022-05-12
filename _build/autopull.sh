754670fb46bd8cc028b35950de2e0f29458387a4
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2022-05-12 21:09:34 +0100

    Added _build/utopull.sh

---
+++ _build/autopull.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# git pull from the folder this script belongs.
+# If there is a change, clear traffice server cache.
+# This script is supposed to be executed by a root cron job.
+
+# cd where this script is
+cd $(dirname "$0")
+
+echo "$(date --iso-8601=seconds) GH fetch"
+
+git fetch
+h1=$(git rev-parse HEAD)
+h2=$(git rev-parse @{u})
+
+if [[ $h1 != $h2 ]]; then
+  echo "$(date --iso-8601=seconds) GH merge + TF clear"
+  git merge && service trafficserver stop && traffic_server -Cclear && service trafficserver start
+  if [[ $? -eq 0 ]]; then
+    echo "done"
+  else
+    echo "failed"
+  fi
+fi
