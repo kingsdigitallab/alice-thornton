3fd3c0e2067c455832a0a4c34b5d5592362fcce8
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2022-10-20 23:26:19 +0100

    Generated DTS for all pages of all books, using the develop branch.

--- apps/dtsstaticgen/settings.js
+++ apps/dtsstaticgen/settings.js
@@ -6,7 +6,7 @@ module.exports = {
   local: `${__dirname}/../../dts.json`,
   // Absolute URL of the Static DTS entry point where the responses will be uploaded.
   // Must end with a .json exension.
-  target: `https://raw.githubusercontent.com/kingsdigitallab/alice-thornton/viewer/dts.json`,
+  target: `https://raw.githubusercontent.com/kingsdigitallab/alice-thornton/develop/dts.json`,
   // target: `http://localhost:5500/alice-thornton/dts.json`,
   // array of Document output formats (see DTS spec.)
   formats: ["html"],
