3281aa458cdc4a90148ef4065fb5aef1b52c327b
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2023-01-10 13:19:17 +0000

    preparing the DTS server to work from github corpus

--- apps/dtsserver/settings.js
+++ apps/dtsserver/settings.js
@@ -1,6 +1,7 @@
 module.exports = {
   // localPath: "../../texts",
   localPath: "/home/jeff/src/prj/tst/alice-thornton/texts",
+  // localPath: "https://github.com/kingsdigitallab/alice-thornton/tree/edition/texts",
   // The stable and base of the URI for most ids (mainly collections & documents).
   // Only used for identifiction, it may not be accessible on the web.
   baseUri: "https://thornton.kdl.kcl.ac.uk/dts/ids/",
