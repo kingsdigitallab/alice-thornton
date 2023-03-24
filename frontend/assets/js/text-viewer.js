481c1991e8f358ec0dd297c53bd84d6e65b22bbe
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2023-03-24 12:38:55 +0000

    moved all viewer styles to separate sheet.

--- frontend/assets/js/text-viewer.js
+++ frontend/assets/js/text-viewer.js
@@ -44,9 +44,9 @@ function setUpTextViewer() {
           {
             selectors: {
               source: {
+                "http://localhost:3000": "AT DTS (local:3000)",
                 "https://raw.githubusercontent.com/kingsdigitallab/alice-thornton/dts/dts.json":
                   "AT SDTS (github)",
-                "http://localhost:3000": "AT DTS (local:3000)",
                 // format=X is ignored, will always return TEI
                 "https://dev.chartes.psl.eu/api/nautilus/dts": "PSL Chartes",
                 // CORS prevents call to Endpoint
